d426caafc1bdc08e4c19b5bd63fc77ae269ee438
Matthew So <matthew.so@gatech.edu>
2019-02-08 20:26:19 -0500

    Start button added

--- app/src/main/java/com/example/spacetrader/MainActivity.kt
+++ app/src/main/java/com/example/spacetrader/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.spacetrader
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view: View) {
+
     }
 }
