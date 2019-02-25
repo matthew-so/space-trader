04d681791216b8437ef0352d39796a8f23c63a8d
Kyser <kmontalvo3@gatech.edu>
2019-02-25 14:36:39 -0500

    - made ui for universe generation

--- app/src/main/java/com/example/spacetrader/entity/SolarSystem.java
+++ app/src/main/java/com/example/spacetrader/entity/SolarSystem.java
@@ -1,21 +1,33 @@
 package com.example.spacetrader.entity;
 
+import java.util.ArrayList;
+
 public class SolarSystem {
     private String name;
     private techLevel techLev;
     private Resources resourceType;
     private int xCoor;
     private int yCoor;
-    private Planet planet;
+    private ArrayList<Planet> planet;
+    private Planet planetOne;
 
 
-    SolarSystem(String name, techLevel techLev, Resources resourceType, int x, int y, Planet planet){
+    public SolarSystem(String name, techLevel techLev, Resources resourceType, int x, int y, ArrayList<Planet> planet){
         this.name = name;
         this.techLev = techLev;
         this.resourceType = resourceType;
         this.xCoor = x;
         this.yCoor = y;
         this.planet = planet;
+    }
+
+    public SolarSystem(String name, techLevel techLev, Resources resourceType, int x, int y, Planet planetOne){
+        this.name = name;
+        this.techLev = techLev;
+        this.resourceType = resourceType;
+        this.xCoor = x;
+        this.yCoor = y;
+        this.planetOne = planetOne;
     }
 
     public int getxCoor() {
@@ -26,8 +38,12 @@ public class SolarSystem {
         return yCoor;
     }
 
-    public Planet getPlanet() {
+    public ArrayList<Planet> getPlanet() {
         return planet;
+    }
+
+    public Planet getPlanetOne() {
+        return planetOne;
     }
 
     public Resources getResourceType(){
