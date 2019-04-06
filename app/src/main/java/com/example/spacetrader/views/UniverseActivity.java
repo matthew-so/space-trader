3045142fbbb35cb739190fbb1c9e1270861a1870
Kyser <kmontalvo3@gatech.edu>
2019-04-06 16:33:50 -0400

    -changed universeactivity vars to local

--- app/src/main/java/com/example/spacetrader/views/UniverseActivity.java
+++ app/src/main/java/com/example/spacetrader/views/UniverseActivity.java
@@ -7,59 +7,31 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.example.spacetrader.R;
-import com.example.spacetrader.entity.Constants;
 import com.example.spacetrader.entity.Planet;
-import com.example.spacetrader.entity.Resource;
-import com.example.spacetrader.entity.SolarSystem;
-import com.example.spacetrader.entity.TechLevel;
 import com.example.spacetrader.entity.Universe;
 
 
-import java.util.ArrayList;
 
 
 public class UniverseActivity extends AppCompatActivity {
 
-    //TODO: Put all this creation stuff in universe constructor and just call constructor with no params
-    /**
-     * planets
-     */
-    private Planet Earth;
-    private Planet Mars;
-    private Planet Saturn;
-    private Planet Mercury;
-    private Planet Venus;
-    private Planet Uranus;
-    private Planet Pluto;
-    private Planet Ares;
-    private Planet Gelo;
-    private Planet Vulcan;
-    private Planet Swirl;
-
-    /**
-     * Text views for each solarsystem generated
-     */
-    private TextView SolarSystemView0;
-    private TextView SolarSystemView1;
-    private TextView SolarSystemView2;
-    private TextView SolarSystemView3;
-    private TextView SolarSystemView4;
-    private TextView SolarSystemView5;
-    private TextView SolarSystemView6;
-    private TextView SolarSystemView7;
-    private TextView SolarSystemView8;
-    private TextView SolarSystemView9;
     public static Universe universe;
-
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_universe);
+
+        TextView SolarSystemView0;
+        TextView SolarSystemView1;
+        TextView SolarSystemView2;
+        TextView SolarSystemView3;
+        TextView SolarSystemView4;
+        TextView SolarSystemView5;
+        TextView SolarSystemView6;
+        TextView SolarSystemView7;
+        TextView SolarSystemView8;
+        TextView SolarSystemView9;
 
         SolarSystemView0 =  findViewById(R.id.SolarSystemView0);
         SolarSystemView1 =  findViewById(R.id.SolarSystemView1);
