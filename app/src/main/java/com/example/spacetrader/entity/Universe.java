40c74495f663ac33c6bdd22ff64590f6ae8618fc
evisalguero <evisalguero@gmail.com>
2019-04-15 08:13:18 -0400

    add canTravelTomethod

--- app/src/main/java/com/example/spacetrader/entity/Universe.java
+++ app/src/main/java/com/example/spacetrader/entity/Universe.java
@@ -25,4 +25,26 @@ public class Universe {
     public String getName() {
         return name;
     }
+
+    public ArrayList<String> canTravelTo(Player player, SolarSystem ss) {
+        ArrayList<String> validSolarList;
+        validSolarList = new ArrayList<>();
+        int x1 = ss.getxCoor();
+        int y1 = ss.getyCoor();
+        for (int i = 0; i < this.solarSystems.size(); i++) {
+            SolarSystem otherSS = this.solarSystems.get(i);
+            int x2 = otherSS.getxCoor();
+            int y2 = otherSS.getyCoor();
+            if ((player.getShipType().getFuel() - Math.hypot(x2-x1, y2-y1)) >= 0) {
+                validSolarList.add(solarSystems.get(i).getName());
+
+            }
+        }
+        return validSolarList;
+    }
+
+
+
+
+
 }
